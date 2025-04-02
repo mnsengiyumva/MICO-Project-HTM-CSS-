bfa42c27da476e1b138d8e7310bb8eb405fe2843
mnsengiyumva <133344917+mnsengiyumva@users.noreply.github.com>
2025-04-01 22:48:55 -0300

    hangman art

--- main.py
+++ main.py
@@ -108,3 +108,68 @@ for l in s:
         su += (upper.index(l)+1)*2
     if l in dig:
         su+= digi.index(l)
+
+import random
+words = ('Banana', 'orange', 'apple')
+
+hangman_art = {0:("   ",
+                  "   ",
+                  "   "),
+               1:(" o ",
+                  "   ",
+                  "   "),
+               2:(" o ",
+                  " | ",
+                  "   "),
+               3:(" o ",
+                  "/| ",
+                  "  "),
+               4:(" o ",
+                  "/|\\",
+                  "   ",
+                  ),
+               5:(" o ",
+                  "/|\\",
+                  "/  ",
+                  ),
+               6:(" o ",
+                  "/|\\",
+                  "/ \\  ",
+                  )}
+def display_man(wrong_guesses):
+    print("****************")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("****************")
+def display_hint(hint):
+    print(" ".join(hint))
+def display_answer(answer):
+    print(" ".join(answer))
+def main():
+    answer = random.choice(words)
+    hint = ["_"]*len(answer)
+    wrrong_guesses =0
+    guessed_letters = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrrong_guesses)
+        display_hint(hint)
+        guess = input('Enter a letter: ').lower()
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalind In put")
+            continue
+        if guess in guessed_letters:
+            print(f'Our {guess} is already Guessed')
+            continue
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+        else:
+            wrrong_guesses +=1
+
+if __name__ == "__main__":
+    main()
+print()
+

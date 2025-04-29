0dd1465a07631667140f2d60c49e0fa1f1ff0fcd
mnsengiyumva <133344917+mnsengiyumva@users.noreply.github.com>
2025-04-28 22:25:23 -0300

    javafiles

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -1,1 +1,11 @@
-import matplotlib
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Expense Tracker")
+root.geometry("500x500")
+
+label = tk.Label(root, text="Welcome to the Expense Tracker", font=("Arial",17))
+label.pack(pady=20)
+
+root.mainloop()
--- Practice.java
+++ Practice.java
@@ -1,6 +1,39 @@
+
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Practice {
 
     public static void main(String[] args){
         System.out.println("Hello world");
+
+        // if statements
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Mico");
+        names.add("Billy");
+        names.add("Tom");
+        System.out.println(names);
+
+        // File handling ---> Writing a file
+
+        try{
+            FileWriter myFile = new FileWriter("myjavafile.txt");
+            myFile.write("This is my first java file, out of more I will creat.");
+            myFile.close();
+            System.out.println("Files written Successfully");
+
+
+        } catch(IOException e){
+            System.out.println("An Error occured");
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
     }
 }

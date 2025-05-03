12d2b46441d94046a3afc15414a56c93633dff7e
mnsengiyumva <133344917+mnsengiyumva@users.noreply.github.com>
2025-05-03 20:38:17 -0300

    new changes

--- practice.js
+++
@@ -1,9 +0,0 @@
-// Determining fibonacci numbers
-function fibNumbers(n){
-    let fibNum = [0,1,1]
-    for (let i = 1; i<n; i++){
-        fibNum.push(fibnum[i]+fibnum[i+1])
-    }
-    return fibNum
-}
-console.log(10)
